046e2db0750d3a1e36679d834ca7c983deebe4eb
Simon Thörnqvist <simon@plug-n-trade.com>
2019-01-04 11:04:46 +0100

    http supervisor

---
+++ lib/defb/http/supervisor.ex
@@ -0,0 +1,26 @@
+defmodule Defb.HTTP.Supervisor do
+  use Supervisor
+  require Logger
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, opts, opts)
+  end
+
+  def init(opts) do
+    port = Keyword.get(opts, :port, 4000)
+
+    children = [
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: OpenAPI.HTTP.Router,
+        options: [port: port, timeout: 70_000]
+      )
+    ]
+
+    Logger.info(fn -> "[#{__MODULE__}] :: Starting HTTP server on port #{port}" end,
+      ansi_color: :magenta
+    )
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
