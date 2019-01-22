d1c069368609552e1d1930424bdfba22d1a44de0
Simon Thörnqvist <simon@plug-n-trade.com>
2019-01-22 15:50:19 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Defb.MixProject do
   def project do
     [
       app: :defb,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
